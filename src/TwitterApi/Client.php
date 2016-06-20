938cddb9a35eb1738856fd60fa47d85cf33545e8
Hokuto Shinoda <mohimovi@gmail.com>
2016-06-21 01:14:47 +0900

    名前衝突を回避

--- src/TwitterApi/Client.php
+++ src/TwitterApi/Client.php
@@ -2,17 +2,17 @@
 
 namespace App\TwitterApi;
 
-use Abraham\TwitterOAuth\TwitterOAuth as Client;
+use Abraham\TwitterOAuth\TwitterOAuth as ApiClient;
 
 class Client
 {
     const DEFAULT_RETRY_LIMIT = 3;
 
-    private $client;
+    private $apiClient;
 
     public function __construct()
     {
-        $this->client = new Client(
+        $this->apiClient = new ApiClient(
             getenv('TWITTER_API_KEY'),
             getenv('TWITTER_API_SECRET'),
             getenv('TWITTER_API_ACCESS_TOKEN'),
@@ -22,10 +22,10 @@ class Client
 
     public function request(string $method, string $endpoint, array $options = [], int $retryLimit = self::DEFAULT_RETRY_LIMIT)
     {
-        $client = $this->client;
+        $apiClient = $this->apiClient;
 
-        return RequestRetryHelper::request(function () use ($method, $client, $endpoint, $options) {
-            return $client->{$method}($endpoint, $options);
+        return RequestRetryHelper::request(function () use ($method, $apiClient, $endpoint, $options) {
+            return $apiClient->{$method}($endpoint, $options);
         }, $retryLimit);
     }
 }

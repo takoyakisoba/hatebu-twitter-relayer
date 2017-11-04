cbbd624499083b2270fc26ddbe1e9d2c7e812d23
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2017-11-04 09:25:37 +0900

    fix(controller): fix status code when relay

--- controller/RelayController.php
+++ controller/RelayController.php
@@ -13,16 +13,25 @@ class RelayController extends BaseController
         $parser = $this->container['hatena_bookmark.event_notification_parser'];
         $eventEntity = $parser->parse($this->request);
         if (!$eventEntity->verify()) {
-            return;
+            return $this->response
+                ->withStatus(400)
+                ->getBody()
+                ->write('pre-shared key mismatched.');
         }
         if (!$eventEntity->validate()) {
-            return;
+            return $this->response
+                ->withStatus(400)
+                ->getBody()
+                ->write('request validation failed.');
         }
 
         /** @var Relayer $relayer */
         $relayer = $this->container['hatebu_twitter_relay.relayer'];
         $relayer->relay($eventEntity);
 
-        return;
+        return $this->response
+            ->withStatus(200)
+            ->getBody()
+            ->write('ok');
     }
 }

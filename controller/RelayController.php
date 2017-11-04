500c208143fbf667302c20411296d9775bbe487c
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2017-11-04 09:12:20 +0900

    chore(controller): add type hint

--- controller/RelayController.php
+++ controller/RelayController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\HatebuTwitterRelay\Relayer;
+use App\HatenaBookmark\EventNotificationParser;
+
 class RelayController extends BaseController
 {
     public function processIncomingWebhook()
     {
+        /** @var EventNotificationParser $parser */
         $parser = $this->container['hatena_bookmark.event_notification_parser'];
         $eventEntity = $parser->parse($this->request);
         if (!$eventEntity->verify()) {
@@ -15,6 +19,7 @@ class RelayController extends BaseController
             return;
         }
 
+        /** @var Relayer $relayer */
         $relayer = $this->container['hatebu_twitter_relay.relayer'];
         $relayer->relay($eventEntity);
 
